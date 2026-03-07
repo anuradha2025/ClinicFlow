44f3286c661bf5470f054eb8550538cd14b6e5d3
anuradha2024 <anuradhasanjaya2024@gmail.com>
2026-03-07 11:31:57 +0530

    Initial Commit

---
+++ ClinicFlow/ClinicFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClinicFlowApp.swift
+//  ClinicFlow
+//
+//  Created by COBSCCOMP242P-020 on 2026-03-07.
+//
+
+import SwiftUI
+
+@main
+struct ClinicFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClinicFlow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClinicFlow
+//
+//  Created by COBSCCOMP242P-020 on 2026-03-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
